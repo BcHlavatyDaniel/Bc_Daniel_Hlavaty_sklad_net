5f01a23ced7bfc5bd22cd65ca37b3fb7e5abc1c6
Kello Tomas <kello.tomas@gmail.com>
2019-05-02 11:23:31 +0200

    Item design zmeneny

--- materialApp/DatabaseProj/Settings.cs
+++ materialApp/DatabaseProj/Settings.cs
@@ -6,6 +6,6 @@ namespace DatabaseProj
 {
 	static class Settings
 	{
-		public static string ConnectionString = "Server=dokelu.kst.fri.uniza.sk; port = 3306; Database=wpfdata; Uid=root; password=root; Convert Zero Datetime=True";
+		public static string ConnectionString = "Server=dokelu.kst.fri.uniza.sk; port = 3306; Database=wpfdata; Uid=test; password=test; Convert Zero Datetime=True";
 	}
 }
