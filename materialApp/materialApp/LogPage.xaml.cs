8ea07f8fc12c674408dcf1d8d56c73de037f84c6
BcHlavatyDaniel <45340946+BcHlavatyDaniel@users.noreply.github.com>
2019-05-04 08:55:45 +0200

    Add files via upload

--- materialApp/materialApp/LogPage.xaml.cs
+++ materialApp/materialApp/LogPage.xaml.cs
@@ -40,6 +40,10 @@ namespace materialApp
         CommonActions mCommonActions;
         VideoCapture mCapture;
         ImageViewer mViewer;
+        List<LogTableItem> itemList = new List<LogTableItem>();
+        List<string> nameCmbList = new List<string>(); //Chapem ze toto je napicu riesenie, ale ked tam chcem aj ten empty item, rychlejsie zbuchatelne..
+        List<string> itemCmbList = new List<string>();
+        List<string> typCmbList = new List<string>();
 
         public LogPage(DbActions mDbActions, ImageViewer view, VideoCapture capture)
         {
@@ -53,76 +57,61 @@ namespace materialApp
             mCommonActions = new CommonActions();
             DataSet logData = mDbActions.LoadAllLogs();
             LoadLogGrid(logData);
-            Item_idLogCmb.Items.Insert(0, "");
-            Year_numbersLogCmb.Items.Insert(0, "");
-            TypeCmb.Items.Insert(0, "");
+            nameCmbList.Insert(0, "");
+            itemCmbList.Insert(0, "");
+            typCmbList.Insert(0, "");
             mCapture = capture;
             mViewer = view;
+            logDataGrid.ItemsSource = itemList;
+            Item_idLogCmb.ItemsSource = itemCmbList;
+            Year_numbersLogCmb.ItemsSource = nameCmbList;
+            TypeCmb.ItemsSource = typCmbList;
         }
 
         private void LoadLogGrid(DataSet data)
         {
-            logDataGrid.ItemsSource = null;
-
             data.Tables[0].Columns.Remove("id");
             data.Tables[0].Columns["item_id"].ColumnName = "id tovar";
             data.Tables[0].Columns["user_id"].ColumnName = "id uzivatel";
             data.Tables[0].Columns["type"].ColumnName = "typ zmeny";
             data.Tables[0].Columns["change_text"].ColumnName = "popis zmeny";
 
-            UpdateCmbox_items(data, 2);
-            logDataGrid.ItemsSource = data.Tables[0].DefaultView;
+            itemList.Clear();
+            foreach (DataRow row in data.Tables[0].Rows)
+            {
+                itemList.Add(new LogTableItem
+                {
+                    id_tovar = row["id tovar"].ToString(),
+                    id_uzivatela = row["id uzivatel"].ToString(),
+                    typ_zmeny = row["typ zmeny"].ToString(),
+                    popis = row["popis zmeny"].ToString(),
+                    time = row["time"].ToString(),
+                });
+            }
             logDataGrid.Items.Refresh();
             logDataGrid.UpdateLayout();
-            if (logDataGrid.Columns.Count > 4)
-            {
-                UpdateColumnsWidths();
-            }
+            UpdateCmbox_items(data);
         }
 
-        private void UpdateColumnsWidths()
+        private void UpdateCmbox_items(DataSet data)
         {
-            logDataGrid.Columns[0].Width = 150; //actWidth / 10;//150;
-            logDataGrid.Columns[1].Width = 150; //actWidth / 10;//150;
-            logDataGrid.Columns[2].Width = 150; //actWidth / 10;//150;
-            logDataGrid.Columns[3].Width = 150; //actWidth / 10;//150;
-            logDataGrid.Columns[4].Width = 650; //actWidth / 2.3;//650;
-            logDataGrid.Columns[5].Width = 250; //actWidth / 6;//250;
-            dataGrid_CmbPositionUpdate(logDataGrid, 2);
-        }
-
-        private void UpdateCmbox_items(DataSet data, int type)
-        {
-            if (type == 2)
-            {
                 foreach (DataRow row in data.Tables[0].Rows)
                 {
-                    if (!Year_numbersLogCmb.Items.Contains(row["id uzivatel"].ToString()))
-                        Year_numbersLogCmb.Items.Add(row["id uzivatel"].ToString());
-                    if (!Item_idLogCmb.Items.Contains(row["id tovar"].ToString()))
-                        Item_idLogCmb.Items.Add(row["id tovar"].ToString());
-                    if (!TypeCmb.Items.Contains(row["typ zmeny"].ToString()))
-                        TypeCmb.Items.Add(row["typ zmeny"].ToString());
+                    if (!nameCmbList.Contains(row["id uzivatel"].ToString()))
+                        nameCmbList.Add(row["id uzivatel"].ToString());
+                    if (!itemCmbList.Contains(row["id tovar"].ToString()))
+                        itemCmbList.Add(row["id tovar"].ToString());
+                    if (!typCmbList.Contains(row["typ zmeny"].ToString()))
+                        typCmbList.Add(row["typ zmeny"].ToString());
                 }
-            }
-        }
-
-        private void dataGrid_CmbPositionUpdate(DataGrid grid, int type)
-        {
-            if (type == 2)
-            {
-                DataGridRow gridRow = (DataGridRow)grid.ItemContainerGenerator.ContainerFromIndex(0);
-                if (gridRow == null) return;
-                logDataGrid.Columns[0].DisplayIndex = logDataGrid.Columns.Count - 1; //posunutie popisu 
-            }
         }
 
         private void Item_Log_Open(object sender, RoutedEventArgs e)//MouseDoubleClick="Item_Log_Open"
 		{
-            DataRowView datView = ((DataRowView)logDataGrid.SelectedItem);
-            DataSet itemRow = mDbActions.LoadSpecificItem(datView.Row.ItemArray[0].ToString());
+            LogTableItem item = ((LogTableItem)logDataGrid.SelectedItem);
+            DataSet itemRow = mDbActions.LoadSpecificItem(item.id_tovar);
             DataSet userRow = mDbActions.LoadSpecificUser(itemRow.Tables[0].Rows[0]["user_year"].ToString(), itemRow.Tables[0].Rows[0]["user_numbers"].ToString());
-            Item_details mItemDWindow = new Item_details(datView.Row.ItemArray[0].ToString(), userRow.Tables[0].Rows[0]["first_name"].ToString(), userRow.Tables[0].Rows[0]["second_name"].ToString(), userRow.Tables[0].Rows[0]["year"].ToString() + "-" + userRow.Tables[0].Rows[0]["_numbers"].ToString(), mViewer, mCapture);
+            Item_details mItemDWindow = new Item_details(item.id_tovar, userRow.Tables[0].Rows[0]["first_name"].ToString(), userRow.Tables[0].Rows[0]["second_name"].ToString(), userRow.Tables[0].Rows[0]["year"].ToString() + "-" + userRow.Tables[0].Rows[0]["_numbers"].ToString(), mViewer, mCapture);
             mItemDWindow.Owner = Window.GetWindow(this);//this;
             mItemDWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
             mItemDWindow.ShowDialog();
@@ -153,16 +142,6 @@ namespace materialApp
                 }
             }
             return changed;
-        }
-
-        private void Datagrid_Cmb_Update(object sender, RoutedEventArgs e)
-        {
-            logDataGrid.Items.Refresh();
-            logDataGrid.UpdateLayout();
-            if (logDataGrid.Columns.Count > 4)
-            {
-                UpdateColumnsWidths();
-            }
         }
 
         private void SearchLogs(object sender, RoutedEventArgs e)
