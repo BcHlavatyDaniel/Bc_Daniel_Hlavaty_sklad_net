8cbe0e13c3082c3b8bab4974f215d71192334bdb
BcHlavatyDaniel <45340946+BcHlavatyDaniel@users.noreply.github.com>
2019-05-04 08:56:29 +0200

    Add files via upload

---
+++ materialApp/DatabaseProj/LogTableItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseProj
+{
+    public class LogTableItem
+    {
+        public string id_tovar { get; set; }
+        public string id_uzivatela { get; set; }
+        public string typ_zmeny { get; set; }
+        public string popis { get; set; }
+        public string time { get; set; }
+    }
+}
